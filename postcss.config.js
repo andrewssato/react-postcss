0657dd7b4a88bb58756aea98719576a815365f8b
Andrews Hideiki Neves Sato <andrews.sato@uscmac018.local>
2017-09-04 20:53:35 -0300

    create-react-app with postcss cssnext

---
+++ postcss.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  require('precss'),
+  require('autoprefixer')
+}
---
+++ src/containers/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import style from './style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={style.app}>
+          Test Postcss app
+      </div>
+    );
+  }
+}
+
+export default App;
